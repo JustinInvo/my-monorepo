c43623b6acf1a11b64853eebcd6c99ade5109796
Justin <jusrtin2010@gmail.com>
2025-09-17 14:40:56 -0500

    first commit

---
+++ libs/shared/src/lib/shared.tsx
@@ -0,0 +1,11 @@
+import styles from './shared.module.scss';
+
+export function MyMonorepoShared() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to MyMonorepoShared!</h1>
+    </div>
+  );
+}
+
+export default MyMonorepoShared;
